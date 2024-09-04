6829deb772e8742452d39a2fa670d56bb9e830ef
Binh Le <lebinh190998@gmail.com>
2024-09-04 18:51:24 +0700

    nearest_neighbor_classifier

--- homework1/homework/nearest_neighbor_classifier.py
+++ homework1/homework/nearest_neighbor_classifier.py
@@ -33,7 +33,7 @@ class NearestNeighborClassifier:
         Returns:
             tuple of x and y both torch.Tensor's.
         """
-        raise NotImplementedError
+        return (torch.as_tensor(x), torch.as_tensor(y))       
 
     @classmethod
     def compute_data_statistics(cls, x: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
@@ -48,7 +48,7 @@ class NearestNeighborClassifier:
             tuple of mean and standard deviation of the data.
             Both should have a shape [1, D]
         """
-        raise NotImplementedError
+        return torch.std_mean(x, dim=0, keepdim=True)[::-1]
 
     def input_normalization(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -72,9 +72,8 @@ class NearestNeighborClassifier:
         Returns:
             tuple of the nearest neighbor data point [D] and its label [1]
         """
-        raise NotImplementedError
         x = self.input_normalization(x)
-        idx = ...  # Implement me:
+        idx = torch.argmin( torch.linalg.norm(self.data_normalized-x, dim=1), dim=0 ).item()
         return self.data[idx], self.label[idx]
 
     def get_k_nearest_neighbor(self, x: torch.Tensor, k: int) -> tuple[torch.Tensor, torch.Tensor]:
@@ -90,9 +89,8 @@ class NearestNeighborClassifier:
             data points will be size (k, D)
             labels will be size (k,)
         """
-        raise NotImplementedError
         x = self.input_normalization(x)
-        idx = ...  # Implement me:
+        idx = torch.topk( torch.linalg.norm(self.data_normalized-x, dim=1), k, largest=False).indices
         return self.data[idx], self.label[idx]
 
     def knn_regression(self, x: torch.Tensor, k: int) -> torch.Tensor:
@@ -107,4 +105,7 @@ class NearestNeighborClassifier:
         Returns:
             average value of labels from the k neighbors. Tensor of shape [1]
         """
-        raise NotImplementedError
+        x = self.input_normalization(x)
+        idx = torch.topk( torch.linalg.norm(self.data_normalized-x, dim=1), k, largest=False).indices
+        return torch.mean(self.label[idx])
+
